a44ba0be77431eb000b671cfd3a13fdb6cd7b1e8
RooT_Kim <bdd3452@gmail.com>
2022-05-23 00:21:36 +0900

    Plus Comment on ex_app.js

--- express_prac/exports_test/ex_app.js
+++ express_prac/exports_test/ex_app.js
@@ -6,5 +6,6 @@ for (let i = 0; i < 10; i++) {
 }
 console.log("\n");
 for (let i = 0; i < 10; i++) {
-  console.log(func.fn2());
+  console.log(func.fn2()); //exports된 함수를 통해서 ex_func의 number의 증감처리를 할 수는 있지만(fn2 함수의 기능이 number변수 증감처리.)
+  // console.log(number);//이렇게 직접 ex_funcs의 number에는 접근하지는 못한다.
 }
