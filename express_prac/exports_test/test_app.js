76c7092a1425ee8dc50f41a81719f7f77addefda
RooT_Kim <bdd3452@gmail.com>
2022-05-26 04:28:04 +0900

    Check module.exports and exports

--- express_prac/exports_test/test_app.js
+++ express_prac/exports_test/test_app.js
@@ -1,18 +1,19 @@
 //기본 실행
 //계산기 작성
 // const cal = require("./test_cal.js");
-const cal = require("./test_cal"); //확장자 .js 붙여도 안붙여도 상관없음
+const cal = require('./test_cal'); //확장자 .js 붙여도 안붙여도 상관없음
 // import Cal from "./test_cal"; => import는 무슨 ES모듈 깔아야되는거같음 검색 ㄱㄱ
-const func = require("./test_func");
+const func = require('./test_func');
 
 console.log(cal.add(5, 3));
 console.log(cal.sub(5, 3));
 console.log(cal.mul(5, 3));
 console.log(cal.div(5, 3));
+console.log(cal.rem(5, 3)); //에러 발생
 
-console.log("처음실행", func());
-console.log("두번실행", func());
-console.log("타입 : ", typeof func);
+console.log('처음실행', func());
+console.log('두번실행', func());
+console.log('타입 : ', typeof func);
 // 같은 기능을 여러번 중복적으로 활용
 for (let i = 0; i < 10; i++) {
   console.log(func());
--- express_prac/exports_test/test_cal.js
+++ express_prac/exports_test/test_cal.js
@@ -14,22 +14,48 @@ const mul = (a, b) => a * b;
 //나눗셈
 const div = (a, b) => a / b;
 
+//테스트용(나머지)
+const rem = (a, b) => a % b;
+
+console.log(module.exports);
+console.log(exports);
+console.log(module.exports === exports);
+//초기에 module.exports와 exports는 같다. (같은 공간을 가리킨다.)
+//그렇기 때문에 아래와 같이 기존 객체에 프로퍼티를 추가하는 방식을 사용하면
+//객체의 데이터가 공유된다.
+
 // module.exports.add = add;
 // module.exports.sub = sub;
 // module.exports.mul = mul;
 // module.exports.div = div;
+// 혹은
+// exports.add = add;
+// exports.sub = sub;
+// exports.mul = mul;
+// exports.div = div;
+// module.exports.rem = rem;
+// 어떤 방식을 사용해도 같은 object공간에 속성만 추가하는 방식.
+
+// 하지만 속성 추가가 아닌
+// 아예 새로운 객체를 할당할 경우 둘은 어긋나게 된다.
+
+// module.exports = {
+//   add: add,
+//   sub: sub,
+//   mul: mul,
+//   div: div,
+// };
+module.exports = { add, sub, mul, div };
+exports = { rem };
+// 각자 객체 공간 내에서 데이터가 추가되는게 아니라 새로운 객체가 할당되어
+// 콘솔을 찍어봐도 다른 값이 되었다는 것을 알 수 있다.
+// 그리고 exports는 module.exports의 경로를 참조하는 변수였기 때문에
+// module.exports의 값이 다른 객체로 바뀌는순간 exports가 call by reference하던 경로와 달라지게 된다.
+// 그렇기 때문에 test_app에서 값을 require해도 넘어오지 않는다.
+// module.exports 객체에 속한 프로퍼티들만 넘어온다.
 
 console.log(module.exports);
 console.log(exports);
-module.exports = {
-  add: add,
-  sub: sub,
-  mul: mul,
-  div: div,
-};
-// module.exports = { add, sub, mul, div };
-// exports.cal = { add, sub, mul, div };
-console.log(module.exports);
-console.log(exports);
+console.log(module.exports === exports);
 
 // export default Cal; => export는 무슨 ES모듈 깔아야되는거같음 검색 ㄱㄱ
